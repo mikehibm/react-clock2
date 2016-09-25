588753833f8dc32b2a67c281dfda84447741fa6d
Makoto Ishida <makotoishida@gmail.com>
2016-09-24 15:30:30 -1000

    Copy moment.js as static file.

--- copy.js
+++ copy.js
@@ -2,5 +2,6 @@ const cpx = require("cpx");
 const dest = 'dist/lib/';
 
 cpx.copy('node_modules/es6-shim/es6-shim.min.js', dest);
+cpx.copy('node_modules/moment/min/moment.min.js', dest);
 cpx.copy('node_modules/react/dist/react.js', dest);
 cpx.copy('node_modules/react-dom/dist/react-dom.js', dest);
--- webpack.config.js
+++ webpack.config.js
@@ -37,11 +37,8 @@ module.exports = {
         // ]
     },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
+        "moment": "moment",
         "react": "React",
         "react-dom": "ReactDOM"
     },
